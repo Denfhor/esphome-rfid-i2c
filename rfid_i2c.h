3141b6f4e0044b3a14ded1510af57cad310f669e
Denfhor <159883459+Denfhor@users.noreply.github.com>
2026-09-12 11:40:31 +0200

    Add RFID_I2C class for I2C communication

---
+++ rfid_i2c.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "esphome.h"
+
+class RFID_I2C : public PollingComponent {
+ public:
+  Sensor *value_sensor = new Sensor();
+
+  RFID_I2C() : PollingComponent(100) {}
+
+  void setup() override {}
+
+  void update() override {
+    uint8_t data = 0;
+    if (Wire.requestFrom(0x28, 1) == 1) {
+      data = Wire.read();
+      value_sensor->publish_state(data);
+    }
+  }
+};
