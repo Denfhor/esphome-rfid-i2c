0d548334b0565b8bda3d7cca0ac671d68c661438
Denfhor <159883459+Denfhor@users.noreply.github.com>
2026-09-12 12:27:18 +0200

    Modify RFID_I2C to handle UID reading
    Updated RFID_I2C::update to read UID as hex string.

--- rfid_i2c.cpp
+++ rfid_i2c.cpp
@@ -11,11 +11,25 @@ void RFID_I2C::setup() {
 }
 
 void RFID_I2C::update() {
-  uint8_t data = 0;
+  const uint8_t UID_LENGTH = 4;   // cambia se il tuo modulo usa UID più lunghi
+  uint8_t buffer[UID_LENGTH];
 
-  if (Wire.requestFrom(0x28, 1) == 1) {
-    data = Wire.read();
-    this->value_sensor->publish_state(data);
+  Wire.requestFrom(0x28, UID_LENGTH);
+
+  if (Wire.available() == UID_LENGTH) {
+    for (int i = 0; i < UID_LENGTH; i++) {
+      buffer[i] = Wire.read();
+    }
+
+    // Converti in stringa esadecimale
+    char uid_hex[UID_LENGTH * 2 + 1];
+    for (int i = 0; i < UID_LENGTH; i++) {
+      sprintf(uid_hex + (i * 2), "%02X", buffer[i]);
+    }
+
+    uid_sensor->publish_state(uid_hex);
+
+    ESP_LOGI(TAG, "UID letto: %s", uid_hex);
   }
 }
 
