16cd7ccfbbd5b90508b7a834c02121a98ed6393f
Denfhor <159883459+Denfhor@users.noreply.github.com>
2026-09-12 11:46:13 +0200

    Update rfid_i2c.cpp

--- rfid_i2c.cpp
+++ rfid_i2c.cpp
@@ -1,1 +1,23 @@
 #include "rfid_i2c.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace rfid_i2c {
+
+static const char *const TAG = "rfid_i2c";
+
+void RFID_I2C::setup() {
+  ESP_LOGI(TAG, "RFID I2C setup complete");
+}
+
+void RFID_I2C::update() {
+  uint8_t data = 0;
+
+  if (Wire.requestFrom(0x28, 1) == 1) {
+    data = Wire.read();
+    this->value_sensor->publish_state(data);
+  }
+}
+
+}  // namespace rfid_i2c
+}  // namespace esphome
